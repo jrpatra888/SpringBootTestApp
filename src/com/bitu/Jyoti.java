e6b2dac433eaaea15650c71e7f0d693fc0e643ce
SSJ046 <jenasudhanshu15@gmail.com>
2023-07-31 12:57:25 +0530

    Well 101- Hello Git

---
+++ src/com/bitu/Jyoti.java
@@ -0,0 +1,21 @@
+package com.bitu;
+
+public class Jyoti {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start");
+		int a=10;
+		a=a++ + ++a - ++a+a++;
+		if(a>55)
+		{
+			System.out.println("Valid");
+		}
+		else
+		{
+			System.out.println("Invalid");
+		}
+		System.out.println("Done");
+
+	}
+
+}
